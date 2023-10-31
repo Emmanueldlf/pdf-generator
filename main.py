43cbf5f2cebbf523513f50bbc73f4d73a2e5af3c
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-10-31 23:58:09 +0900

    code to create a one page pdf

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from pyfpdf import FPDF
+from fpdf import FPDF
 
-pdf = FPDF(orientation="P",unit="mn",format="A4")
+pdf = FPDF(orientation="P",unit="mm",format="A4")
 
 print(type(pdf))
 
-pdf.add_pages()
+pdf.add_page()
+
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1, border=1)
+pdf.cell(w=0, h=12, txt="Hi There!", align="L", ln=1, border=1)
 
 pdf.output("output.pdf")
