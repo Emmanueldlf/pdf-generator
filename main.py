c03650a83f19fcba25e90a38e5cb294999ce8a71
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-11-08 23:45:42 +0900

    refactored footer and header code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P",unit="mm",format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 print(type(pdf))
 
@@ -10,12 +11,25 @@ df = pd.read_csv("topics.csv")
 for index,row in df.iterrows():
     pdf.add_page()
 
+    #Header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=1)
     pdf.line(10, 21, 200, 21)
 
-    for i in range(row["pages"]):
+     #Footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    for i in range(row["Pages"]):
         pdf.add_page()
+
+        #Footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=1)
 
 pdf.output("output.pdf")
